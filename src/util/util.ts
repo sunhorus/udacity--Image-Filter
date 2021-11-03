7f53ecc25d3a2fab6504de8c47e3a7cb12fe87f0
Mostafa Said <sunhorus@gmail.com>
2021-11-03 14:43:52 +0200

    working

--- src/util/util.ts
+++ src/util/util.ts
@@ -17,7 +17,7 @@ export async function filterImageFromURL(inputURL: string): Promise<string>{
         .quality(60) // set JPEG quality
         .greyscale() // set greyscale
         .write(__dirname+outpath, (img)=>{
-            resolve(__dirname+outpath);
+            resolve(__dirname+outpath); 
         });
     });
 }
